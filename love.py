f6f1a4c6b0035f1d56ea579c3ed8870cc416b9f9
TrekCodes <87086486+TrekCodes@users.noreply.github.com>
2022-05-31 15:25:44 +0530

    Add files via upload

---
+++ love.py
@@ -0,0 +1,25 @@
+import turtle
+
+from main import my_goto
+
+t = turtle.Turtle()
+turtle.title("Love Shape Programming")
+screen = turtle.Screen()
+screen.bgcolor("white")
+t.color("yellow", "red")
+t.begin_fill()
+t.speed(2)
+t.pensize(3)
+t.fillcolor("red")
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.setheading(60)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
+if __name__ == '__main__':
+    my_goto(-100, -200)
+    turtle.write('Heart Program', font=("Arial", 24, "bold"),)
+    turtle.mainloop()
